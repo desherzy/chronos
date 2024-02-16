385fa980db9e47d861278d2341dab3fd1c276449
Medvid <114774321+desherzy@users.noreply.github.com>
2024-02-16 13:10:29 +0200

    +userService, +calendarService
    +userService, +calendarService

--- services/calendarService.js
+++ services/calendarService.js
@@ -3,13 +3,25 @@ const Calendar = require('../models/Calendar');
 const Permission = require('../models/Permission');
 const UserCalendar = require('../models/UserCalendar');
 const CalendarDto = require('../dtos/CalendatDto');
+const CalendarEvents = require('../models/CalendarEvent');
+const ApiError = require('../exceptions/apiError');
 
 class CalendarService {
 
     async checkPermissions(userId, calendarId) { 
-//if user has permission to edit (permission_id 1 or permission_id 2) return true,
-// if not: throw ApiError.badRequest('User has no permissions to work with this calendar.');
-
+        //if user has permission to edit (permission_id 1 or permission_id 2) return true,
+        // if not: throw ApiError.badRequest('User has no permissions to work with this calendar.');
+        try {
+            const userCalendar = UserCalendar.findAll({where: {calendar_id: calendarId, user_id: userId}});
+            if (userCalendar.permission_id == 1 || userCalendar.permission_id == 2) {
+                return true;
+            } else {
+                throw ApiError.badRequest('User has no permissions to work with this calendar.');
+            }
+        } catch (error) {
+            console.error('Failed to check permission:', error);
+            throw error;
+        }
     }
 
     async getUserCalendars(userId) {   //get calendars,then pass them to dto and return dtos 
@@ -29,38 +41,64 @@ class CalendarService {
     }
 
     async createCalendar(userId, name, description, color) { //create calendar and return dto
-        
-        
-        
-        //после создания перед return добавить айди юзера и календаря и permission_id 1 (это права создателя) в модель UserCalendars 
+        try {
+            const calendar = await Calendar.create({name: name, description: description, color: color});
+            const calendarDto = new CalendarDto(calendar);
+
+            //после создания перед return добавить айди юзера и календаря и permission_id 1 (это права создателя) в модель UserCalendars 
+            const userCalendar = await UserCalendar.create({user_id: userId, calendar_id: calendarDto.id, permission_id: 1});
+
+            return calendarDto;
+        } catch (error) {
+            console.error('Calendar not created:', error);
+            throw error;
+        }
     }
 
     async updateCalendar(updatedFields, calendarId) { //look what rows has updated fields and update, return dto
-        
-        /* get calendar from db (const calendar = ...) and then:
+        try {
+            const calendar = Calendar.findAll({where: {calendar_id: calendarId}});
+            if (!calendar) {
+                throw ApiError.badRequest('Calendar is not found');
+            }
 
-        if (!calendar) {
-            throw ApiError.badRequest('Calendar is not found');
-        } 
-        if (updatedFields.name) {
-            calendar.name = updatedFields.name;
+            if (updatedFields.name) {
+                calendar.name = updatedFields.name;
+            }
+            if (updatedFields.description) {
+                calendar.calendar = updatedFields.description;
+            }
+            if (updatedFields.color) {
+                calendar.color = updatedFields.color;
+            }
+
+            //permission (?)
+            const userCalendar = await UserCalendar.findAll({where: {calendar_id: calendarId}});
+            const calendarDto = new CalendarDto(calendar, userCalendar.permission_id);
+            return calendarDto;
+        } catch (error) {
+            console.error('Calendar not updated:', error);
+            throw error;
         }
-        if (updatedFields.description) {
-            calendar.description = updatedFields.description;
-        }
-        if (updatedFields.color) {
-            calendar.color = updatedFields.color;
-        } 
-
-            ...
-
-        const calendarDto = new CalendarDto(calendar, permission (нужно достать выше permission для календаря)) 
-        return calendarDto*/
     }
 
     async deleteCalendar(id) { //delete, returns nothing
         //перед тем как удалять нужно очистить сначала CalendarEvents а потом UserCalendar с айдишкой этого календаря
         //а потом только удалять с таблицы календарь, если так не сделать то будет ошибка при удалении тк этот айди есть в других таблицах.
+        try {
+            const calendar = await Calendar.findAll({where: {id: id}});
+            if (!calendar) {
+                throw ApiError.badRequest('Calendar exists');
+            } else {
+                const calendarEvents = await CalendarEvents.destroy({where: {calendar_id: id}});
+                const userCalendar = await UserCalendar.destroy({where: {calendar_id: id}});
+                calendar = await Calendar.destroy({where: {id: id}});
+                console.log('Calendar[' + id + '] deleted');
+            }
+        } catch (error) {
+            console.error('Calendar not deleted', error);
+            throw error;
+        }
     }   
 
 }
--- services/userService.js
+++ services/userService.js
@@ -1,74 +1,160 @@
-
+const User = require('../models/User');
+const UserDto = require('../dtos/UserDto');
+const Invitation = require('../models/Invitation');
+const ApiError = require('../exceptions/apiError');
+const bcrypt = require('bcrypt');
+const UserCalendar = require('../models/UserCalendar');
+const UserEvent = require('../models/UserEvent');
+const Links = require('../models/Links');
+const Tokens = require('../models/Tokens');
 
 class UserService {
     
     async getUsers() {
-     //   const users = find all users from db
-     /*   const userDTOs = users.map(user => new UserDto(user));
-          return userDTOs; */
+        try {
+          const users = await User.findAll();
+          if (users) {
+            const userDtos = users.map(user => new UserDto(user));
+            console.log(userDtos);
+            return userDtos;
+          } else {
+            console.log('Users not found');
+            throw ApiError.notFound('Users not found'); 
+          }
+        } catch (error) {
+          console.error('Error getting users:', error);
+          throw error;
+        }
+        
     }
 
     async getUser(id) {
-        //get user
-       /* const userDto = new UserDto(user);
-        return userDto; */
+        try {
+          const user = await User.findOne({where: {id: id}});
+          if (user != undefined) {
+            const userDto = new UserDto(user, 1); // where we get session_id?
+            return userDto;
+          } else {
+            console.log('User not found');
+            throw ApiError.badRequest('User not found'); 
+          }
+        } catch (error) {
+          console.error('Error getting user:', error);
+          throw error;
+        }
     }
-
 
     async updateUser(id, updatedFields) {
-      //find user. const user = ...
-    /*    if (!user) {
-            throw ApiError.badRequest('User is not found');
-        }
-        if (updatedFields.login) {
+        try {
+          const user = await User.findOne({where: {id: id}});
+          if (!user) {
+            console.log('User not found');
+            throw ApiError.badRequest('User not found'); 
+          }
+
+          if (updatedFields.login) {
             user.login = updatedFields.login;
-        }
-        if (updatedFields.email) {
+          }
+          if (updatedFields.email) {
             user.email = updatedFields.email;
+            // need to confirm email?
+          }
+
+          const userDto = new UserDto(user);
+          return userDto;
+        } catch (error) {
+          console.error('Error updating user:', error);
+          throw error;
         }
-        then save and throw updated user model to DTO
-        const userDto = new UserDto(updatedUser);
-        return userDto; */
     }
 
-    async deleteUser(id) {
+    async deleteUser(id) { 
        // firstly delete all records with this userId from db (invitation, userCalendar, userEvent, links, tokens tables)
        // and then delete user from user table
+
+      try {
+        const user = await User.findAll({where: {id: id}});
+        if (!user) {
+          throw ApiError.badRequest('User exists');
+        } else {
+          const inviter = await Invitation.destroy({where: {inviter_user_id: id}});
+          const invited = await Invitation.destroy({where: {invited_user_id: id}});
+          
+          const userCalendar = await UserCalendar.destroy({where: {user_id: id}});
+          const userEvent = await UserEvent.destroy({where: {user_id: id}});
+          
+          const links = await Links.destroy({where: {user_id: id}});
+          const tokens = await Tokens.destroy({where: {user_id: id}});
+          
+          user = await User.destroy({where: {id: id}});
+          console.log('User[' + id + '] deleted');
+        }
+      } catch (error) {
+        console.error('Error deliting user:', error);
+        throw error;
+      }
     }
 
     async uploadUserPhoto(id, photoPath) { //just save photoPath variable to database (user.profile_image field in db)
-      //  const user = find by id
-
-      /*  if (!user) {
-            throw ApiError.badRequest('User is not found');
-        } */
-      /* user.profile_image = photoPath; 
-      then save user and throw to DTO
-        const userDto = new UserDto(updatedUser);
-
-        return userDto; */
+        try {
+          const user = await User.findOne({where: {id: id}});
+          if (!user) {
+            console.log('User not found');
+            throw ApiError.badRequest('User not found'); 
+          }
+          if (photoPath) {
+            user.profileImage = photoPath;
+          }
+          const userDto = new UserDto(user);
+          return userDto;
+        } catch (error) {
+          console.error('Error uploading user photo:', error);
+          throw error;
+        }
     }
 
     async deleteUserPhoto(id) { //the same as above, but set profile_image to null;
- //  const user = find by id
-
-      /*  if (!user) {
-            throw ApiError.badRequest('User is not found');
-        } */
-      /* user.profile_image = null; 
-      then save user and throw to DTO
-        const userDto = new UserDto(updatedUser);
-
-        return userDto; */
+        try {
+          const user = await User.findOne({where: {id: id}});
+          if (!user) {
+            console.log('User not found');
+            throw ApiError.badRequest('User not found'); 
+          } else {
+            user.profileImage = null;
+            const userDto = new UserDto(user);
+            return userDto;
+          }
+        } catch (error) {
+          console.error('Error deliting user:', error);
+          throw error;
+        }
     }
 
-    async changePass(id, passwords) {
+    async changePass(id, passwords) { // ???
        //return nothing, passwords object contain newPassword and oldPassword, so we need to check if passwords.oldPassword is right password, if yes save passwords.newPassword
        //before checking we need to hash old and compare to the hash from db (password field)
        //const hashNewPassword = await bcrypt.hash(passwords.newPassword, 3); hash new and then save
+      try {
+        const user = await User.findOne({where: {id: id}});
+        if (!user) {
+          console.log('User not found');
+          throw ApiError.badRequest('User not found'); 
+        }
 
+        let hashedOldPassword = await bcrypt.hash(passwords.oldPassword, 3);
+
+        if (user.password != hashedOldPassword) {
+          console.log('Passwords not matched');
+          throw ApiError.badRequest('Passwords not matched');
+        } else {
+          let hashedNewPassword = await bcrypt.hash(passwords.newPassword, 3);
+          user.password = hashedNewPassword;
+        }
+      } catch (error) {
+        console.error('Error updating user:', error);
+        throw error;
+      }
     }
-
 }
 
 module.exports = new UserService();
